ec58c692e7c1642305413350917efe7d2802ba64
Eddie Dover <ed@eddiedover.dev>
2025-12-09 15:37:13 -0800

    chore: add coverage to PRs

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "json-summary"],
       include: ["src/scripts/hacking/**/*.js"],
       exclude: [
         "src/scripts/hacking/HackingGameRegistry.js",
